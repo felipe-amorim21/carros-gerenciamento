2647077d797bc005f8256cffc88e362e67e693c5
Felipe Amorim <felipe21fab@gmail.com>
2024-08-15 21:22:31 -0300

    Add carro service and controller

---
+++ backend-carrogerenciamento/src/main/java/com/felipe/carros/carrogerenciamento/controller/CarroController.java
@@ -0,0 +1,47 @@
+package com.felipe.carros.carrogerenciamento.controller;
+
+import com.felipe.carros.carrogerenciamento.entity.Carro;
+import com.felipe.carros.carrogerenciamento.service.CarroService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/carro")
+public class CarroController {
+
+    CarroService carroService;
+
+    public CarroController(CarroService carroService){
+        this.carroService = carroService;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Carro> save(@RequestBody Carro carro){
+        return new ResponseEntity<>(carroService.save(carro), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Carro> update(@RequestBody Carro carro, @PathVariable long id){
+        return new ResponseEntity<>(carroService.update(id, carro), HttpStatus.OK);
+    }
+
+    @GetMapping("/listAll")
+    public ResponseEntity<List<Carro>> findAll(){
+        return new ResponseEntity<>(carroService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<Carro> findById(@PathVariable long id){
+        return new ResponseEntity<>(carroService.findById(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("deleteById/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable long id){
+        carroService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ backend-carrogerenciamento/src/main/java/com/felipe/carros/carrogerenciamento/service/CarroService.java
@@ -0,0 +1,43 @@
+package com.felipe.carros.carrogerenciamento.service;
+
+import com.felipe.carros.carrogerenciamento.entity.Carro;
+import com.felipe.carros.carrogerenciamento.repository.CarroRepository;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CarroService {
+    CarroRepository carroRepository;
+
+    public CarroService(CarroRepository carroRepository){
+        this.carroRepository = carroRepository;
+    }
+
+
+
+    public Carro save(Carro carro){
+        return carroRepository.save(carro);
+    }
+
+    public Carro update(long id, Carro carro){
+        if(!carroRepository.existsById(id)){
+            throw new RuntimeException("Carro com id: " + id + " Não encontrado!");
+        }
+        carro.setId(id);
+        return carroRepository.save(carro);
+    }
+
+    public Carro findById(long id){
+        return carroRepository.findById(id).orElseThrow(() -> new RuntimeException("Carro com id: "+ id + " Não encontrado!"));
+    }
+
+    public List<Carro> findAll(){
+        return carroRepository.findAll();
+    }
+
+    public void deleteById(long id){
+        carroRepository.deleteById(id);
+    }
+}
